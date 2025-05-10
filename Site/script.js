d21afc7d5807169e9bb499234235eece8f419694
guedespietro5 <guedesdev5@gmail.com>
2025-05-10 16:05:37 -0300

    Initial commit

---
+++ Site/script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem() {
+    const mensagem = document.getElementById('mensagem');
+    mensagem.textContent = "O Vale do Paraíba representa hoje um exemplo de transformação regional baseada em inovação e tecnologia.";
+  }
+  
